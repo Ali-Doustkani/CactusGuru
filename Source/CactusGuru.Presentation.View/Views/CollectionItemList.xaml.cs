71abef61a7d7b13733a07b01b08ad9be6dda673d
Ali Doustkani <ali.doostkany@gmail.com>
2018-09-11 13:36:48 +0430

    ugly context menu of collection list created

--- Source/CactusGuru.Presentation.View/Views/CollectionItemList.xaml.cs
+++ Source/CactusGuru.Presentation.View/Views/CollectionItemList.xaml.cs
@@ -1,42 +1,10 @@
-﻿using CactusGuru.Presentation.ViewModel.ViewModels.CollectionItemListViewModels;
-using DevExpress.Xpf.Bars;
-using System.Windows;
-
-namespace CactusGuru.Presentation.View.Views
+﻿namespace CactusGuru.Presentation.View.Views
 {
     public partial class CollectionItemList 
     {
         public CollectionItemList()
         {
             InitializeComponent();
-        }
-
-        private void Edit_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (ViewModel() == null) return;
-            ViewModel().EditCurrentCollectionItemCommand.Execute(null);
-          //  grid.RefreshRow(grid.View.FocusedRowHandle);
-        }
-
-        private void Delete_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (ViewModel() != null)
-                ViewModel().DeleteCurrentCollectionItemCommand.Execute(null);
-        }
-
-        private void Copy_OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            ViewModel().CopyNameCommand.Execute(null);
-        }
-
-        private void GotoImageGallery_OnItemClick(object sender, ItemClickEventArgs e)
-        {
-            ViewModel().GotoImageGallaryCommand.Execute(null);
-        }
-
-        private CollectionItemListViewModel ViewModel()
-        {
-            return (CollectionItemListViewModel)DataContext;
         }
     }
 }
