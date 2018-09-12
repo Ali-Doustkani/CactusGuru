35fb43cf96fa9aac0404f02c6e8a7a28535877d9
Ali Doustkani <ali.doostkany@gmail.com>
2018-09-12 18:22:27 +0430

    lookupedits changed with combos

--- Source/CactusGuru.Presentation.View/Utils/FocusHandler.cs
+++ Source/CactusGuru.Presentation.View/Utils/FocusHandler.cs
@@ -1,6 +1,5 @@
-﻿using DevExpress.Xpf.Editors;
-using DevExpress.Xpf.Grid.LookUp;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace CactusGuru.Presentation.View.Utils
@@ -22,6 +21,11 @@ namespace CactusGuru.Presentation.View.Utils
             typeof(FocusHandler),
             new PropertyMetadata(TraverseOnEnterChanged));
 
+        public static readonly DependencyProperty AutoOpenDropDownProperty = DependencyProperty.RegisterAttached("AutoOpenDropDown",
+            typeof(bool),
+            typeof(FocusHandler),
+            new PropertyMetadata(AutoOpenDropDownChanged));
+
         public static bool GetHasFocus(UIElement obj)
         {
             return (bool)obj.GetValue(HasFocusProperty);
@@ -40,6 +44,16 @@ namespace CactusGuru.Presentation.View.Utils
         public static void SetTraverseOnEnter(UIElement obj, bool value)
         {
             obj.SetValue(TraverseOnEnterProperty, value);
+        }
+
+        public static bool GetAutoOpenDropDown(UIElement obj)
+        {
+            return (bool)obj.GetValue(AutoOpenDropDownProperty);
+        }
+
+        public static void SetAutoOpenDropDown(UIElement obj, bool value)
+        {
+            obj.SetValue(AutoOpenDropDownProperty, value);
         }
 
         private static void HasFocusChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
@@ -68,20 +82,25 @@ namespace CactusGuru.Presentation.View.Utils
                 RemoveControl((FrameworkElement)obj);
         }
 
-        public static void AddControl(FrameworkElement ctrl)
+        private static void AddControl(FrameworkElement ctrl)
         {
-            if (ctrl is LookUpEdit)
-                (ctrl as LookUpEdit).PopupClosed += TabIndexController_PopupClosed;
-            ctrl.PreviewKeyUp += ctrl_PreviewKeyUp;
+            if (ctrl is ComboBox)
+                (ctrl as ComboBox).DropDownClosed += DropDownClosed;
+            ctrl.PreviewKeyDown += ctrl_PreviewKeyDown;
             ctrl.Unloaded += Ctrl_Unloaded;
         }
 
-        public static void RemoveControl(FrameworkElement element)
+        private static void RemoveControl(FrameworkElement element)
         {
-            if (element is LookUpEdit)
-                (element as LookUpEdit).PopupClosed -= TabIndexController_PopupClosed;
-            element.PreviewKeyUp -= ctrl_PreviewKeyUp;
+            if (element is ComboBox)
+                (element as ComboBox).DropDownClosed -= DropDownClosed;
+            element.PreviewKeyDown -= ctrl_PreviewKeyDown;
             element.Unloaded -= Ctrl_Unloaded;
+        }
+
+        private static void DropDownClosed(object sender, System.EventArgs e)
+        {
+            GotoNextControl(sender);
         }
 
         private static void Ctrl_Unloaded(object sender, RoutedEventArgs e)
@@ -89,13 +108,7 @@ namespace CactusGuru.Presentation.View.Utils
             RemoveControl((FrameworkElement)sender);
         }
 
-        private static void TabIndexController_PopupClosed(object sender, ClosePopupEventArgs e)
-        {
-            if (e.CloseMode == PopupCloseMode.Normal)
-                GotoNextControl(sender);
-        }
-
-        private static void ctrl_PreviewKeyUp(object sender, KeyEventArgs e)
+        private static void ctrl_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
                 GotoNextControl(sender);
@@ -103,7 +116,31 @@ namespace CactusGuru.Presentation.View.Utils
 
         private static void GotoNextControl(object ctrl)
         {
-            ((UIElement)ctrl).MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+            var form = Window.GetWindow((DependencyObject)ctrl);
+            var element = FocusManager.GetFocusedElement(form) as FrameworkElement;
+            if (element != null)
+                element.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+        }
+
+        private static void AutoOpenDropDownChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue.Equals(false)) return;
+            var combo = obj as ComboBox;
+            if (combo == null) return;
+            combo.GotFocus += Combo_GotFocus;
+            combo.Unloaded += Combo_Unloaded;
+        }
+
+        private static void Combo_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ((ComboBox)sender).IsDropDownOpen = true;
+        }
+
+        private static void Combo_Unloaded(object sender, RoutedEventArgs e)
+        {
+            var combo = (ComboBox)sender;
+            combo.GotFocus -= Combo_GotFocus;
+            combo.Unloaded -= Combo_Unloaded;
         }
     }
 }
