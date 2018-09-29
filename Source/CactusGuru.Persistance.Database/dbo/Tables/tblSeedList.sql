f50265341d5cc61516cfb3e0ff41d03b7491d9cb
Ali Doustkani <ali.doostkany@gmail.com>
2018-09-29 11:30:46 +0330

    seedlist tables removed from the schema

--- Source/CactusGuru.Persistance.Database/dbo/Tables/tblSeedList.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[tblSeedList] (
-    [Id]          UNIQUEIDENTIFIER NOT NULL,
-    [Name]        NVARCHAR (50)    NOT NULL,
-    [PublishDate] DATETIME         NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
